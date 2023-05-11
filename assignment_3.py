5e515c85057772000acd516621ca90017ab5a7d8
Veera Raghunatha Reddy Naguru <51355468+veeraraghu@users.noreply.github.com>
2023-05-11 11:48:05 +0100

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed May 10 11:21:06 2023
 
-@author: Acer
-"""
-
+#Importing necessary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,25 +12,26 @@ import errors as err
 import importlib
 
 
-def read_data(file_path):
-    data=pd.read_csv(file_path,skiprows=4)
+def reading_data(filepath):
+    data=pd.read_csv(filepath,skiprows=4)
     data=data.set_index('Country Name',drop=True)
     data=data.loc[:,'1960':'2021']
     return data
-def transpose(data):
+def transposed_data(data):
     data_tr=data.transpose()
     
     return data_tr
 
-def corr_scattermatrix(data):
+def correlation_and_scattermatrix(data):
     corr = data.corr()
     print(corr) 
     plt.figure(figsize=(10, 10))
-    # fig, ax = plt.subplots()
     plt.matshow(corr, cmap='coolwarm')
-    # setting ticks to column names
+    
+    # xticks and yticks for corr matrix
     plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
     plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.title('Correlation between Years and Countries over GDP per capita')
     plt.colorbar()
     plt.show()
 
@@ -43,50 +40,46 @@ def corr_scattermatrix(data):
     
     return
 
-def n_clusters(data,data_norm,a,b):
+def cluster_number(df,df_normalised):
     
-    n_clusters=[]
-    cluster_score=[]
-    
-    
+    clusters=[]
+    scores=[]
+    # loop over number of clusters
     for ncluster in range(2, 10):
         
-        # set up the clusterer with the number of expected clusters
+        # Setting up clusters over number of clusters
         kmeans = cluster.KMeans(n_clusters=ncluster)
 
-        # Fit the data, results are stored in the kmeans object
-        kmeans.fit(data_norm)     # fit done on x,y pairs
+        # Cluster fitting
+        kmeans.fit(df_normalised)
+        lab = kmeans.labels_
 
-        labels = kmeans.labels_
+        # Silhoutte score over number of clusters
+        print(ncluster, skmet.silhouette_score(df, lab))
         
-        # extract the estimated cluster centres
-        cen = kmeans.cluster_centers_
-
-        # calculate the silhoutte score 
-        print(ncluster, skmet.silhouette_score(data, labels))
+        clusters.append(ncluster)
+        scores.append(skmet.silhouette_score(df, lab))
         
-        n_clusters.append(ncluster)
-        cluster_score.append(skmet.silhouette_score(data, labels))
+    clusters=np.array(clusters)
+    scores=np.array(scores)
         
-    n_clusters=np.array(n_clusters)
-    cluster_score=np.array(cluster_score)
-        
-    best_ncluster=n_clusters[cluster_score==np.max(cluster_score)]
+    best_ncluster=clusters[scores==np.max(scores)]
+    print()
     print('best n clusters',best_ncluster[0])
     
     return best_ncluster[0]
         
         
     
-def clusters_and_centers(df_norm,ncluster,a,b):
+def clusters_and_centers(df,ncluster,y1,y2):
     # set up the clusterer with the number of expected clusters
     kmeans = cluster.KMeans(n_clusters=ncluster)
 
     # Fit the data, results are stored in the kmeans object
-    kmeans.fit(df_norm)     # fit done on x,y pairs
+    kmeans.fit(df)
 
     labels = kmeans.labels_
-    df_norm['labels']=labels
+    df['labels']=labels
     # extract the estimated cluster centres
     cen = kmeans.cluster_centers_
 
@@ -94,145 +87,102 @@ def clusters_and_centers(df_norm,ncluster,a,b):
     xcen = cen[:, 0]
     ycen = cen[:, 1]
 
-
     # cluster by cluster
     plt.figure(figsize=(8.0, 8.0))
 
     cm = plt.cm.get_cmap('tab10')
-    plt.scatter(df_norm[a], df_norm[b], 10, labels, marker="o", cmap=cm)
-    plt.scatter(xcen, ycen, 45, "k", marker="d")
-    plt.xlabel(f"GDP({a})")
-    plt.ylabel(f"GDP({b})")
+    plt.scatter(df[y1], df[y2], 10, labels, marker="o", cmap=cm)
+    plt.scatter(xcen, ycen, 45, "k", marker="s")
+    plt.xlabel(f"GDP per capita({y1})")
+    plt.ylabel(f"GDP per capita({y2})")
+    plt.title('Clusters of Countries over GDP per capita in 1970 and 2020')
     plt.show()
-
+ 
     print(cen)
 
     
-    return 
-    
+    return df
 
-df_co2 = read_data("gdp_per_capita.csv")
+
+def logistic(t, n0, g, t0): 
+    """Calculates the logistic function with scale factor n0 
+    and growth rate g"""
+    
+    f = n0 / (1 + np.exp(-g*(t - t0)))
+    
+    return f
+
+
+def forecast_gdp(df,country,start_year,end_year):
+    df=df.loc[:,country]
+    df=df.dropna(axis=0) 
+
+    df_gdp=pd.DataFrame()
+
+    df_gdp['Year']=pd.DataFrame(df.index)
+    df_gdp['GDP']=pd.DataFrame(df.values)
+    df_gdp["Year"] = pd.to_numeric(df_gdp["Year"])
+    importlib.reload(opt)
+
+    param, covar = opt.curve_fit(logistic, df_gdp["Year"], df_gdp["GDP"], 
+                                 p0=(1.2e12, 0.03, 1990.0))
+
+    sigma = np.sqrt(np.diag(covar))
+
+    year = np.arange(start_year, end_year)
+    forecast = logistic(year, *param)
+    low, up = err.err_ranges(year, logistic, param, sigma)
+    plt.figure()
+    plt.plot(df_gdp["Year"], df_gdp["GDP"], label="GDP")
+    plt.plot(year, forecast, label="forecast",color='k')
+    plt.fill_between(year, low, forecast, color="red", alpha=0.7)
+    plt.fill_between(year, forecast, up, color="green", alpha=0.7)
+    plt.xlabel("year")
+    plt.ylabel("GDP per capita (USD)")
+    plt.legend(loc='upper left')
+    plt.title(f'GDP per capita forecast for {country}')
+    plt.savefig(f'{country}.png',bbox_inches='tight',dpi=300)
+    plt.show()
+
+    gdp2030 = logistic(2030, *param)/1e9
+
+    low, up = err.err_ranges(2030, logistic, param, sigma)
+    sig = np.abs(up-low)/(2.0 * 1e9)
+    print()
+    print("GDP 2030", gdp2030*1e9, "+/-", sig*1e9)
+
+df_co2 = reading_data("gdp_per_capita.csv")
 print(df_co2.describe())
 
-df_co2_tr=transpose(df_co2)
+df_co2_tr=transposed_data(df_co2)
 print(df_co2_tr.head())
 
 df_co3 = df_co2[["1970", "1980", "1990", "2000", "2010",'2020']]
 print(df_co3.describe())
 
-corr_scattermatrix(df_co3)
-a="1990"
-b="2020"
-df_ex = df_co3[[a, b]]  # extract the two columns for clustering
+correlation_and_scattermatrix(df_co3)
+year1="1970"
+year2="2020"
 
-
-df_ex = df_ex.dropna()  # entries with one nan are useless
+# Extract the two columns for clustering
+df_ex = df_co3[[year1, year2]]  
+df_ex = df_ex.dropna()  
 print(df_ex.head())
 
 # normalise, store minimum and maximum
 df_norm, df_min, df_max = ct.scaler(df_ex)
 
 print()
-print("n  score")
-# loop over number of clusters
-ncluster=n_clusters(df_ex,df_norm,a,b)
+print("Number of Clusters and Scores")
+ncluster=cluster_number(df_ex,df_norm)
 
-clusters_and_centers(df_norm, ncluster,a,b)
+df_norm=clusters_and_centers(df_norm, ncluster,year1,year2)
 
-clusters_and_centers(df_ex, ncluster,a,b)
+df_ex=clusters_and_centers(df_ex, ncluster,year1,year2)
 
-print(df_ex[df_ex['labels']==ncluster-1])
-
-
-df_co2_tr=df_co2_tr.loc[:,'Brazil']
-df_co2_tr=df_co2_tr.dropna(axis=0) 
-print('Transpose')
-print(df_co2_tr.head())
+print(df_ex[df_ex['labels']==ncluster-2])
 
 
-df_gdp=pd.DataFrame()
+forecast_gdp(df_co2_tr,'Monaco',1970,2031)
 
-df_gdp['Year']=pd.DataFrame(df_co2_tr.index)
-df_gdp['GDP']=pd.DataFrame(df_co2_tr.values)
-
-print(df_gdp.head())
-
-df_gdp.plot("Year", "GDP")
-plt.show()
-
-
-df_gdp["Year"] = pd.to_numeric(df_gdp["Year"])
-
-# param, covar = opt.curve_fit(exponential, df_gdp["Year"], df_gdp["GDP"], p0=(1.2e12, 0.03))
-
-
-def logistic(t, n0, g, t0): 
-    """Calculates the logistic function with scale factor n0 and growth rate g"""
-    
-    f = n0 / (1 + np.exp(-g*(t - t0)))
-    
-    return f
-
-importlib.reload(opt)
-
-param, covar = opt.curve_fit(logistic, df_gdp["Year"], df_gdp["GDP"], 
-                             p0=(1.2e12, 0.03, 1990.0))
-
-sigma = np.sqrt(np.diag(covar))
-
-df_gdp["fit"] = logistic(df_gdp["Year"], *param)
-
-df_gdp.plot("Year", ["GDP", "fit"])
-plt.show()
-
-print("turning point", param[2], "+/-", sigma[2])
-print("GDP at turning point", param[0]/1e9, "+/-", sigma[0]/1e9)
-print("growth rate", param[1], "+/-", sigma[1])
-
-
-
-year = np.arange(1960, 2031)
-forecast = logistic(year, *param)
-
-plt.figure()
-plt.plot(df_gdp["Year"], df_gdp["GDP"], label="GDP")
-plt.plot(year, forecast, label="forecast")
-
-plt.xlabel("year")
-plt.ylabel("GDP")
-plt.legend()
-plt.show()
-
-import errors as err
-
-low, up = err.err_ranges(year, logistic, param, sigma)
-
-plt.figure()
-plt.plot(df_gdp["Year"], df_gdp["GDP"], label="GDP")
-plt.plot(year, forecast, label="forecast")
-
-plt.fill_between(year, low, up, color="yellow", alpha=0.7)
-plt.xlabel("year")
-plt.ylabel("GDP")
-plt.legend()
-plt.show()
-
-print(logistic(2030, *param)/1e9)
-print(err.err_ranges(2030, logistic, param, sigma))
-
-# assuming symmetrie estimate sigma
-gdp2030 = logistic(2030, *param)/1e9
-
-low, up = err.err_ranges(2030, logistic, param, sigma)
-sig = np.abs(up-low)/(2.0 * 1e9)
-print()
-print("GDP 2030", gdp2030, "+/-", sig)
-
-
-
-
-
-
-
-
-
+forecast_gdp(df_co2_tr,'United States',1960,2031) 
